86b1672ed47cf4961d097af78a860d4ec0b3729d
Oaik <omarashrf15@gmail.com>
2017-04-05 17:17:27 +0200

    Bootstrap II (Elzero)

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function() {
+	"use strict";
+	$(window).scroll(function() {
+		var navbar = $(".navbar");
+		if ( $(window).scrollTop() >= navbar.height() ? navbar.addClass("scrolled") : navbar.removeClass("scrolled"));
+	});	
+	// Tabs
+	$(".tabs li").click(function() {
+		// Add active class to active link
+		$(this).addClass("active").siblings().removeClass("active");
+		// Hide all divs on click
+		$(".tabs .tabs-content > div").hide();
+		//show div
+		$('.' + $(this).data("class")).show();
+	});
+
+});
